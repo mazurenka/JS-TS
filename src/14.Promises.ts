4707bc7f41be59fa18ceba2e7c93717ff13f8146
mazurenka <an.drej@mail.ru>
2022-02-24 18:39:16 +0100

    14 - JS/TS for students - Promises

--- src/14.Promises.ts
+++ src/14.Promises.ts
@@ -3,19 +3,26 @@ import axios from "axios";
 const findUserInDB = (id: number) => {
 
 }
-
-
+// pending
 const promise1 = axios.get('https://google.com')
 promise1.then((data) => {
     console.log(data)
 })
 
-const promise2 = findUserInDB(1)
-promise2.then( (user) => {
-    console.log(user)
-})
+const promise2 = findUserInDB(2)
+promise2
+    .then((user) => {
+        console.log(user)
+    })
+    .catch((error) => {
+        console.warn(error)
+    })
+    .finally(() => {
+        console.log('finish')
+    })
 
-console.log('finish')
+
+
 
 
 
