1ad8309deaf22a2033c0942e34a1e4083e6d4cc6
mazurenka <an.drej@mail.ru>
2022-02-24 19:21:11 +0100

    14 - JS/TS for students - Promises

--- src/14.Promises.ts
+++ src/14.Promises.ts
@@ -9,16 +9,26 @@ promise1.then((data) => {
     console.log(data)
 })
 
-const promise2 = findUserInDB(2)
+const promise2 = findUserInDB(1)
 promise2
     .then((user) => {
         console.log(user)
     })
-    .catch((error) => {
-        console.warn(error)
-    })
-    .finally(() => {
+// .catch((error) => {
+//     console.warn(error)
+// })
+// .finally(() => {
+//     console.log('finish')
+// })
+
+const otherPromise = Promise.all([promise1, promise2])
+
+otherPromise
+    .then(() => {
         console.log('finish')
+    })
+    .catch(() => {
+        console.log('initialization failed')
     })
 
 
