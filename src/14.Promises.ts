165dbce9af6858e864613341d8b76b421dbe4058
mazurenka <an.drej@mail.ru>
2022-02-25 11:10:31 +0100

    14 - JS/TS for students - Promises

--- src/14.Promises.ts
+++ src/14.Promises.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+import {users} from "./08_Associative arrays";
 
 const findUserInDB = (id: number) => {
 
@@ -28,6 +31,67 @@ otherPromise2
     .then((res) => {
         console.log(res)
     })
+
+const resolvedPromise = Promise.resolve(100)
+resolvedPromise
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+// const rejectedPromise = Promise.reject({message: 'Some error'})
+//
+// rejectedPromise
+//     .then(data => console.log(data))
+//     .catch(error => console.warn(error))
+
+const userApi = {
+    getAllUsers() {
+        Promise.resolve({name: "Andre"})
+    }
+}
+
+const pr = userApi.getAllUsers()
+console.log(pr.then(users = > console.log(users))
+
+
+const usersApi = {
+    getAllUsers() {
+        return Promise.resolve({name: 'D'})
+    },
+    login(login, password) {
+        if (login !== '123' && password !== '123') {
+            return Promise.reject({message: 'Incorrect log and pass'})
+        } else {
+            return Promise.resolve({name: 'A'})
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
