2f9b173e420a29418d8a269f46b8a57a25462f65
mazurenka <an.drej@mail.ru>
2022-02-26 15:46:04 +0100

    14 - JS/TS for students - Promises

--- src/14.Promises.ts
+++ src/14.Promises.ts
@@ -1,90 +1,101 @@
-import axios from "axios";
 import {Simulate} from "react-dom/test-utils";
-import error = Simulate.error;
-import {users} from "./08_Associative arrays";
-
-const findUserInDB = (id: number) => {
-
-}
-// pending
-const promise1 = axios.get('https://google.com')
-promise1.then((data) => {
-    console.log(data)
-})
-
-const promise2 = findUserInDB(1)
-promise2
-    .then((user) => {
-        console.log(user)
-    })
-// .catch((error) => {
-//     console.warn(error)
-// })
-// .finally(() => {
-//     console.log('finish')
-// })
-
-//const otherPromise = Promise.all([promise1, promise2])
-const otherPromise2 = Promise.allSettled([promise1, promise2])
-
-otherPromise2
-    .then((res) => {
-        console.log(res)
-    })
-
-const resolvedPromise = Promise.resolve(100)
-resolvedPromise
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
-
-// const rejectedPromise = Promise.reject({message: 'Some error'})
 //
-// rejectedPromise
+// const findUserInDB = (id: number) => {
+//
+// }
+// // pending
+// const promise1 = axios.get('https://google.com')
+// promise1.then((data) => {
+//     console.log(data)
+// })
+//
+// const promise2 = findUserInDB(1)
+// promise2
+//     .then((user) => {
+//         console.log(user)
+//     })
+// // .catch((error) => {
+// //     console.warn(error)
+// // })
+// // .finally(() => {
+// //     console.log('finish')
+// // })
+//
+// //const otherPromise = Promise.all([promise1, promise2])
+// const otherPromise2 = Promise.allSettled([promise1, promise2])
+//
+// otherPromise2
+//     .then((res) => {
+//         console.log(res)
+//     })
+//
+// const resolvedPromise = Promise.resolve(100)
+// resolvedPromise
 //     .then(data => console.log(data))
-//     .catch(error => console.warn(error))
+//     .catch(error => console.log(error))
+//
+// // const rejectedPromise = Promise.reject({message: 'Some error'})
+// //
+// // rejectedPromise
+// //     .then(data => console.log(data))
+// //     .catch(error => console.warn(error))
+//
+// const userApi = {
+//     getAllUsers() {
+//         Promise.resolve({name: "Andre"})
+//     }
+// }
+//
+// const pr = userApi.getAllUsers()
+// console.log(pr.then(users = > console.log(users))
+//
+// const usersApi = {
+//     getAllUsers() {
+//         return Promise.resolve({name: 'D'})
+//     },
+//     login(login, password) {
+//         if (login !== '123' && password !== '123') {
+//             return Promise.reject({message: 'Incorrect log and pass'})
+//         } else {
+//             return Promise.resolve({name: 'A'})
+//         }
+//     }
+// }
+//
+// const promise5 = findUserInDB(1)
+//
+// //const promise5_5 = promise5.then((user) => console.log(user))
+//
+// axios.get('https://google.com')
+//     .then(res => res.data)
+//     .then(data => console.log(data))
+//
+// const makeGoogleRequest = () => {
+//     const pr = axios.get('https://google.com')
+//     const pr2 = pr.then(res => res.data)
+//     return pr2
+// }
+//
+// makeGoogleRequest().then(data => console.log(data))
+//
+// const makeGoogleRequest2 = () => {
+//     return axios.get('https://google.com')
+//         .then(res => res.data)
+// }
+//
+// findUserInDB(1).then((user: any) => {
+//     console.log(user.name)
+// })
 
-const userApi = {
-    getAllUsers() {
-        Promise.resolve({name: "Andre"})
-    }
+function getNumber() {
+    return Math.random()
 }
 
-const pr = userApi.getAllUsers()
-console.log(pr.then(users = > console.log(users))
+const n1 = getNumber()
+const n2 = getNumber()
 
-const usersApi = {
-    getAllUsers() {
-        return Promise.resolve({name: 'D'})
-    },
-    login(login, password) {
-        if (login !== '123' && password !== '123') {
-            return Promise.reject({message: 'Incorrect log and pass'})
-        } else {
-            return Promise.resolve({name: 'A'})
-        }
-    }
-}
-
-const promise5 = findUserInDB(1)
-
-//const promise5_5 = promise5.then((user) => console.log(user))
-
-axios.get('https://google.com')
-    .then(res => res.data)
-    .then(data => console.log(data))
-
-const makeGoogleRequest = () => {
-    const pr = axios.get('https://google.com')
-    const pr2 = pr.then(res => res.data)
-    return pr2
-}
-
-makeGoogleRequest().then(data => console.log(data))
-
-const makeGoogleRequest2 = () => {
-    return axios.get('https://google.com')
-        .then(res => res.data)
-}
+console.log(n1)
+console.log(n2)
 
 
 
